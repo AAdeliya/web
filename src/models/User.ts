58d82e102a0c3821267f2f50b6a424fc7ce047ae
firstPush <adel.sdet2@gmail.com>
2025-09-03 21:05:50 -0500

    updated

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,7 +12,7 @@ export class User {
   get (propName: string): (number | string) {
     return this.data[propName];
   }
-
+//////
   set(update: UserProps): void {
     Object.assign(this.data, update);
 
